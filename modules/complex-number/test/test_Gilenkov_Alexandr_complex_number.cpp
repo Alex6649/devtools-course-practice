c4c75dae9f7d679c70b52981715a61e6e23bc785
AlexRossi <aleksandr.semenychev@mail.ru>
2018-10-24 13:51:13 +0300

    debug

--- modules/complex-number/test/test_Gilenkov_Alexandr_complex_number.cpp
+++ modules/complex-number/test/test_Gilenkov_Alexandr_complex_number.cpp
@@ -8,11 +8,9 @@ TEST(Gilenkov_Alexandr_ComplexNumberTest, Addition_Is_Associative) {
     ComplexNumber z1(1.4, 2.2);
     ComplexNumber z2(2.6, 5.4);
     ComplexNumber z3(6.9, 8.3);
-    
     // ACT
     ComplexNumber sum1 = z1 + (z2 + z3);
-    ComplexNumber sum2 = (z1 + z2) + z3;
-    
+    ComplexNumber sum2 = (z1 + z2) + z3;    
     // ASSERT
     EXPECT_EQ(sum1, sum2);
 }
@@ -20,12 +18,10 @@ TEST(Gilenkov_Alexandr_ComplexNumberTest, Addition_Is_Associative) {
 TEST(Gilenkov_Alexandr_ComplexNumberTest, Addition_Is_Commutative) {
     // ARRANGE
     ComplexNumber z1(1.0, 2.0);
-    ComplexNumber z2(3.4, 5.6);
-    
+    ComplexNumber z2(3.4, 5.6);    
     // ACT
     ComplexNumber sum1 = z1 + z2;
-    ComplexNumber sum2 = z2 + z1;
-    
+    ComplexNumber sum2 = z2 + z1;    
     // ASSERT
     EXPECT_EQ(sum1, sum2);
 }
@@ -33,12 +29,10 @@ TEST(Gilenkov_Alexandr_ComplexNumberTest, Addition_Is_Commutative) {
 TEST(Gilenkov_Alexandr_ComplexNumberTest, Multiple_Is_Commutative) {
     // ARRANGE
     ComplexNumber z1(1.0, 2.0);
-    ComplexNumber z2(3.4, 5.6);
-    
+    ComplexNumber z2(3.4, 5.6);    
     // ACT
     ComplexNumber mult1 = z1 * z2;
-    ComplexNumber mult2 = z2 * z1;
-    
+    ComplexNumber mult2 = z2 * z1;    
     // ASSERT
     EXPECT_EQ(mult1, mult2);
 }
@@ -48,11 +42,9 @@ TEST(Gilenkov_Alexandr_ComplexNumberTest,
     // ARRANGE
     ComplexNumber z1(1.0, 2.0);
     ComplexNumber z2(-1.0, -2.0);
-    ComplexNumber z3(0.0, 0.0);
-    
+    ComplexNumber z3(0.0, 0.0);    
     // ACT
-    ComplexNumber sum1 = z1 + z2;
-    
+    ComplexNumber sum1 = z1 + z2;    
     // ASSERT
     EXPECT_EQ(sum1, z3);
 }
@@ -60,8 +52,7 @@ TEST(Gilenkov_Alexandr_ComplexNumberTest,
 TEST(Gilenkov_Alexandr_ComplexNumberTest, Devide_To_Null) {
     // ARRANGE
     ComplexNumber z1(1.2, 2.3);
-    ComplexNumber null(0, 0);
-    
+    ComplexNumber null(0, 0);    
     // ASSERT
     ASSERT_ANY_THROW(z1 / null);
 }
